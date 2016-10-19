c9b39b56a845ad8b8f5212b4063b0e84c5fee1b3
Teidata <spa@teidata.com>
2016-10-19 19:46:53 +0100

    Update waypoints.js

--- waypoints.js
+++ waypoints.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 // Nuevo waypoint
 var waypoint = new Waypoint({
-  // Elemento objetivo
   element: document.getElementById('tres'),
-  // Función disparada
   handler: function(direction) {
-    alert('¡Has llegado!');
+    console.log('¡Has pasado por aquí!');
+    if (direction == 'down') {
+      console.log('¡Hacia abajo!');
+      this.destroy();                     // Sólo lo detectará una vez
+    }
   }
 });
 });
