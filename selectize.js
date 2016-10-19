58bed760d6bf57f6e90b725ff681899df00e14ff
Teidata <spa@teidata.com>
2016-10-19 17:32:26 +0100

    Update selectize.js

--- selectize.js
+++ selectize.js
@@ -1,10 +1,19 @@
-$('#input-tags').selectize({
-    delimiter: ',',
-    persist: false,
-    create: function(input) {
-      return { 
-        value: input,
-        text: input 
+
+
+$(document).ready(function() {
+  $('#input-tags').selectize({
+      delimiter: ',',
+      persist: false,
+      create: function(input) {
+        return { 
+          value: input,
+          text: input 
+        }
       }
-    }
+  });
+  // return ['tag1', 'tag2', …]
+  function viewTags() {
+    tags = document.getElementById('input-tags');
+    console.log(tags.value.split(','));
+  }
 });
