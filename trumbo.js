764351bf5d101cb713c04f7790c2d124b5fb2b39
Teidata <spa@teidata.com>
2016-10-19 18:53:41 +0100

    Update trumbo.js

--- trumbo.js
+++ trumbo.js
@@ -1,7 +1,7 @@
 /* Javascript */
 $(document).ready(function() {
   $('.editor').trumbowyg({
-      //svgPath: '/icons/icons.svg', // path of icons.svg
+      svgPath: 'icons.svg', // path of icons.svg
       // Habitualmente, se indica la ruta del SVG en este parámetro, pero
       // en codepen, simplemente pegaremos el contenido del SVG en la parte
       // del código HTML
