45670a9b73d0de35bf4936b85ece119c6e855084
Teidata <spa@teidata.com>
2016-10-19 19:24:12 +0100

    Update trumbo.js

--- trumbo.js
+++ trumbo.js
@@ -8,5 +8,17 @@ $(document).ready(function() {
   });
   $('#trumbowyg').trumbowyg('html'); // Obtienes el HTML generado 
     
-
+  $('.editor').trumbowyg({
+    lang: 'es', // Idioma (es.min.js incluido)
+    btns: [
+            ['bold', 'italic', 'underline', 'strikethrough'],
+            ['superscript', 'subscript'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['link', 'insertImage'],
+            ['horizontalRule', 'removeformat'],
+            ['viewHTML'],
+            ['fullscreen']
+          ],
+  });  
 });
