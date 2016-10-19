669ebd923ec5f9680310ab00270bfd48d26135d0
Teidata <spa@teidata.com>
2016-10-19 16:21:54 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Opción 1: Inicializar cookieBar (genérico, en inglés)
+$(document).ready(function() {
+  $.cookieBar();
+});
+
+// Opción 2: Personalizar mensaje (indicar clase)
+$(document).ready(function() {
+  $('.cookie-message').cookieBar({
+    closeButton: '.close'
+  });
+});
