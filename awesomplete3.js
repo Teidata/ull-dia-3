adb40b6331f1e9db475ab87b111753364fa846a4
Teidata <spa@teidata.com>
2016-10-19 17:08:38 +0100

    Create awesomplete3.js

---
+++ awesomplete3.js
@@ -0,0 +1,5 @@
+/* Javascript */
+var input = document.getElementById('datos');
+new Awesomplete(input, {
+    list: ["Ada", "Java", "JavaScript", "Brainfuck", "LOLCODE", "Node.js", "Ruby on Rails"]
+});
